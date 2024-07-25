f1ea2a00e9fcfef0adac0bc8a9ef9f32eeb073d2
michaeljoel <michaeljoelatkinson@gmail.com>
2024-07-25 14:42:58 -0500

    Fix translation error

--- script.js
+++ script.js
@@ -31,22 +31,22 @@ function playGame() {
     if (humanChoice == computerChoice) {
       console.log("Aikodesho! It's a draw!");
     } else if (humanChoice == "guu" && computerChoice == "choki") {
-      console.log("Anata no kachi! You win! Guu beats Choki");
+      console.log("Anata ga shouri! You win! Guu beats Choki");
       humanScore++;
     } else if (humanChoice == "choki" && computerChoice == "paa") {
-      console.log("Anata no kachi! You win! Choki beats Paa");
+      console.log("Anata ga shouri! You win! Choki beats Paa");
       humanScore++;
     } else if (humanChoice == "paa" && computerChoice == "guu") {
-      console.log("Anata no kachi! You win! Paa beats Guu");
+      console.log("Anata ga shouri! You win! Paa beats Guu");
       humanScore++;
     } else if (computerChoice == "guu" && humanChoice == "choki") {
-      console.log("Anata no kachi! You lose! Guu beats Choki");
+      console.log("Anata ga makeru! You lose! Guu beats Choki");
       computerScore++;
     } else if (computerChoice == "choki" && humanChoice == "paa") {
-      console.log("Anata no kachi! You lose! Choki beats Paa");
+      console.log("Anata ga makeru! You lose! Choki beats Paa");
       computerScore++;
     } else if (computerChoice == "paa" && humanChoice == "guu") {
-      console.log("Anata no kachi! You lose! Paa beats Guu");
+      console.log("Anata ga makeru! You lose! Paa beats Guu");
       computerScore++;
     }
   }
