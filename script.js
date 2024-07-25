3302847768a5ea54b87116b93ea9d3f3c3276707
michaeljoel <michaeljoelatkinson@gmail.com>
2024-07-25 14:07:54 -0500

    Create function that dictates round logic

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ function getHumanChoice() {
     console.log("Invalid option!");
   }
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log("Aikodesho! It's a draw!");
+  } else if (humanChoice == "guu" && computerChoice == "choki") {
+    console.log("Anata no kachi! You win! Guu beats Choki");
+    humanScore++;
+  } else if (humanChoice == "choki" && computerChoice == "paa") {
+    console.log("Anata no kachi! You win! Choki beats Paa");
+    humanScore++;
+  } else if (humanChoice == "paa" && computerChoice == "guu") {
+    console.log("Anata no kachi! You win! Paa beats Guu");
+    humanScore++;
+  } else if (computerChoice == "guu" && humanChoice == "choki") {
+    console.log("Anata no kachi! You lose! Guu beats Choki");
+    computerScore++;
+  } else if (computerChoice == "choki" && humanChoice == "paa") {
+    console.log("Anata no kachi! You lose! Choki beats Paa");
+    computerScore++;
+  } else if (computerChoice == "paa" && humanChoice == "guu") {
+    console.log("Anata no kachi! You lose! Paa beats Guu");
+    computerScore++;
+  }
+}
