f1777a98f4181ed23843e3189cf7a798e42ec3fb
michaeljoel <michaeljoelatkinson@gmail.com>
2024-09-25 15:27:24 -0500

    Add user inteface
    Makes playing the game much easier, removes the need to open the console.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
@@ -12,62 +15,60 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  const humanChoice = prompt(
-    "Choose between guu, choki or paa. Jakenpon!"
-  ).toLowerCase();
-  if (humanChoice == "guu" || humanChoice == "choki" || humanChoice == "paa") {
-    return humanChoice;
-  } else {
-    console.log("Invalid option!");
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log("Aikodesho! It's a draw!");
+    presentResult("Aikodesho! It's a draw!");
+  } else if (humanChoice == "guu" && computerChoice == "choki") {
+    console.log("Anata ga shouri! You win! Guu beats Choki");
+    presentResult("Anata ga shouri! You win! Guu beats Choki");
+    humanScore++;
+  } else if (humanChoice == "choki" && computerChoice == "paa") {
+    console.log("Anata ga shouri! You win! Choki beats Paa");
+    presentResult("Anata ga shouri! You win! Choki beats Paa");
+    humanScore++;
+  } else if (humanChoice == "paa" && computerChoice == "guu") {
+    console.log("Anata ga shouri! You win! Paa beats Guu");
+    presentResult("Anata ga shouri! You win! Paa beats Guu");
+    humanScore++;
+  } else if (computerChoice == "guu" && humanChoice == "choki") {
+    console.log("Anata ga makeru! You lose! Guu beats Choki");
+    presentResult("Anata ga makeru! You lose! Guu beats Choki");
+    computerScore++;
+  } else if (computerChoice == "choki" && humanChoice == "paa") {
+    console.log("Anata ga makeru! You lose! Choki beats Paa");
+    computerScore++;
+    presentResult("Anata ga makeru! You lose! Choki beats Paa");
+  } else if (computerChoice == "paa" && humanChoice == "guu") {
+    console.log("Anata ga makeru! You lose! Paa beats Guu");
+    presentResult("Anata ga makeru! You lose! Paa beats Guu");
+    computerScore++;
   }
+  displayScore();
 }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+function presentResult(outcome) {
+  const userOutput = document.querySelector(".userOutput");
+  const results = document.createElement("p");
+  results.textContent = outcome;
 
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-      console.log("Aikodesho! It's a draw!");
-    } else if (humanChoice == "guu" && computerChoice == "choki") {
-      console.log("Anata ga shouri! You win! Guu beats Choki");
-      humanScore++;
-    } else if (humanChoice == "choki" && computerChoice == "paa") {
-      console.log("Anata ga shouri! You win! Choki beats Paa");
-      humanScore++;
-    } else if (humanChoice == "paa" && computerChoice == "guu") {
-      console.log("Anata ga shouri! You win! Paa beats Guu");
-      humanScore++;
-    } else if (computerChoice == "guu" && humanChoice == "choki") {
-      console.log("Anata ga makeru! You lose! Guu beats Choki");
-      computerScore++;
-    } else if (computerChoice == "choki" && humanChoice == "paa") {
-      console.log("Anata ga makeru! You lose! Choki beats Paa");
-      computerScore++;
-    } else if (computerChoice == "paa" && humanChoice == "guu") {
-      console.log("Anata ga makeru! You lose! Paa beats Guu");
-      computerScore++;
-    }
-  }
-
-  for (let i = 0; i < 5; i++) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log(
-      "Your Score: ",
-      humanScore,
-      " Computer's Score: ",
-      computerScore
-    );
-  }
-
-  if (humanScore > computerScore) {
-    console.log("You won the game!");
-  } else {
-    console.log("You lost!");
-  }
+  userOutput.appendChild(results);
 }
 
-playGame();
+function displayScore() {
+  const userOutput = document.querySelector(".userOutput");
+  const score = document.createElement("p");
+  score.textContent = `Player Score: ${humanScore}\nComputer Score: ${computerScore}`;
+
+  userOutput.appendChild(score);
+
+  if (humanScore == 5) {
+    const winner = document.createElement("p");
+    winner.textContent = "You win!";
+    userOutput.appendChild(winner);
+  } else if (computerScore == 5) {
+    const winner = document.createElement("p");
+    winner.textContent = "You lost!";
+    userOutput.appendChild(winner);
+  }
+}
